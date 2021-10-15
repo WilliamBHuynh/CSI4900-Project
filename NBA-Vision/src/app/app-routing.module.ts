1523aa0e540e4b154a3a3852812c72d7f8a8aefa
WilliamBHuynh <whuyn056@uottawa.ca>
2021-10-14 20:48:44 -0400

    Added Home page and routing

--- NBA-Vision/src/app/app-routing.module.ts
+++ NBA-Vision/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ScheduleComponent} from "./schedule/schedule.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'schedule', component: ScheduleComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: HomeComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
