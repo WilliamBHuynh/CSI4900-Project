4ad06eb5eeeb855aa13d746839bf737bd92776ff
WilliamBHuynh <whuyn056@uottawa.ca>
2021-10-17 23:29:58 -0400

    Updated project name

--- NbaVision/karma.conf.js
+++ NbaVision/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/NBA-Vision'),
+      dir: require('path').join(__dirname, './coverage/NbaVision'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- NbaVision/src/app/app.component.spec.ts
+++ NbaVision/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NBA-Vision'`, () => {
+  it(`should have as title 'NbaVision'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NBA-Vision');
+    expect(app.title).toEqual('NbaVision');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NBA-Vision app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('NbaVision app is running!');
   });
 });
