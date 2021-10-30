e2063ca34d8bf0bf434136a0f84c4644d4955943
WilliamBHuynh <whuyn056@uottawa.ca>
2021-10-29 22:39:51 -0400

    Added navigation between schedule days

--- NBA-Vision/src/app/schedule/schedule.component.ts
+++ NBA-Vision/src/app/schedule/schedule.component.ts
@@ -8,28 +8,44 @@ import {GameService} from "../service/game.service";
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+  private data: any;
   today = new Date();
   entries: ScheduleEntry[] = [];
   constructor(private service: GameService) { }
 
   ngOnInit(): void {
-    this.refreshGames();
+    this.refreshSchedule();
   }
 
-  refreshGames(): void {
+  refreshSchedule(): void {
     this.service.getSchedule().subscribe((res: any) => {
-      const data = JSON.parse(res);
-      const convertedCurrentDate = this.today.toISOString().split('T', 1)[0];
-      console.log(convertedCurrentDate)
-      for (let i = 0; i < Object.keys(data.DATE).length; i++) {
-        if (data.DATE[i].split('T', 1)[0] == convertedCurrentDate) {
-          const newEntry: ScheduleEntry = {date: data.DATE[i], homeTeamName: this.convertTeamName(data.HOME[i]),
-            awayTeamName: this.convertTeamName(data.VISITOR[i]), live: data.HOME_PTS[i] != null, homeTeamScore: data.HOME_PTS[i],
-            awayTeamScore: data.VISITOR_PTS[i]};
-          this.addEntry(newEntry);
-        }
-      }
+      this.data = JSON.parse(res);
+      this.renderSchedule();
     });
+  }
+
+  renderSchedule(): void {
+    let convertedCurrentDate = this.today.toISOString().split('T', 1)[0];
+    for (let i = 0; i < Object.keys(this.data.DATE).length; i++) {
+      if (this.data.DATE[i].split('T', 1)[0] == convertedCurrentDate) {
+        const newEntry: ScheduleEntry = {date: this.data.DATE[i], homeTeamName: this.convertTeamName(this.data.HOME[i]),
+          awayTeamName: this.convertTeamName(this.data.VISITOR[i]), live: this.data.HOME_PTS[i] != null,
+          homeTeamScore: this.data.HOME_PTS[i], awayTeamScore: this.data.VISITOR_PTS[i]};
+        this.addEntry(newEntry);
+      }
+    }
+  }
+
+  goBackDay(): void {
+    this.today.setDate(this.today.getDate() - 1);
+    this.entries = [];
+    this.renderSchedule();
+  }
+
+  goForwardDay(): void {
+    this.today.setDate(this.today.getDate() + 1);
+    this.entries = [];
+    this.renderSchedule();
   }
 
   addEntry(entry: ScheduleEntry) {
