5b1f6570d5c2a553c6091b6a08eb99b6789eceda
WilliamBHuynh <whuyn056@uottawa.ca>
2021-11-01 22:11:47 -0400

    Added unit tests to schedule page

--- NBA-Vision/src/app/schedule/schedule.component.spec.ts
+++ NBA-Vision/src/app/schedule/schedule.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScheduleComponent } from './schedule.component';
+import {ScheduleEntry} from "./schedule-entry";
 
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
@@ -21,5 +22,19 @@ describe('ScheduleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show final score', () => {
+    const newEntry: ScheduleEntry = {homeTeamName: "TOR", awayTeamName: "LAC", live: true, homeTeamScore: 100,
+      awayTeamScore: 88, date: new Date()};
+    component.addEntry(newEntry);
+    expect(component).toContain(newEntry.homeTeamScore);
+  });
+
+  it('should show no score', () => {
+    const newEntry: ScheduleEntry = {homeTeamName: "TOR", awayTeamName: "LAC", live: false, homeTeamScore: 0,
+      awayTeamScore: 0, date: new Date()};
+    component.addEntry(newEntry);
+    expect(component).toContain(0);
   });
 });
