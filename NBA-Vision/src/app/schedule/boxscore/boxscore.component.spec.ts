b56ab56858963ff096d0a0d61f4aed8c99ecd7d2
WilliamBHuynh <whuyn056@uottawa.ca>
2021-12-18 11:53:50 -0500

    Added more tests

--- NBA-Vision/src/app/schedule/boxscore/boxscore.component.spec.ts
+++ NBA-Vision/src/app/schedule/boxscore/boxscore.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BoxscoreComponent } from './boxscore.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
+import {BoxscoreEntry} from "../../boxscore/boxscore-entry";
 
 describe('BoxscoreComponent', () => {
   let component: BoxscoreComponent;
@@ -28,5 +29,22 @@ describe('BoxscoreComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add entry to entries list', () => {
+    component.addEntry(new BoxscoreEntry("Scottie Barnes", 36, 18, 50, 13, 57, 14, 4, 37,
+        35, 8, 77, 6, 3, 4, 1, 2, 1, 4, 9, "Toronto Raptors"));
+    expect(component.entries.length).toEqual(1);
+  });
+
+  it('should update selected home and away team values on click', () => {
+    const newHomeAbv = 'BKN';
+    const newAwayAbv = 'LAL';
+    component.homeTeamAbv = newHomeAbv;
+    component.clickHomeTeam();
+    expect(component.selectedTeam).toEqual(newHomeAbv);
+    component.awayTeamAbv = newAwayAbv;
+    component.clickAwayTeam();
+    expect(component.selectedTeam).toEqual(newAwayAbv);
   });
 });
