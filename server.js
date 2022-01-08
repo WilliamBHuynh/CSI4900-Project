13de5e71d4733b2ee68b38a9a320871fd28cb458
WilliamBHuynh <whuyn056@uottawa.ca>
2022-01-08 17:03:32 -0500

    Updated server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/nba-vision'));
+app.use(express.static(__dirname + '/dist/NBA-Vision'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/nba-vision/index.html'));});
+    '/dist/NBA-Vision/index.html'));});
 app.listen(process.env.PORT || 8080);
